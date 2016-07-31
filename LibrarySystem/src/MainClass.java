86a1734d7629aa570e174051972f496a6357de40
AK <aniket.kudtarkar@gmail.com>
2016-08-01 10:45:14 +1200

    Added a new object to the class

--- LibrarySystem/src/MainClass.java
+++ LibrarySystem/src/MainClass.java
@@ -64,8 +64,8 @@ public class MainClass
 	Book b1 = new Book("79456", "JAVA", "John Grisham", 2);
 	b1.displayInfo();
 	
-/*	Book b2 = new Book("123456", "Oracle", "ED Burns", 3);
-	b2.displayInfo();*/
+	Book b2 = new Book("123456", "Oracle", "ED Burns", 3);
+	b2.displayInfo();
 	}
 
 }
